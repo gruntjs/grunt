2e53ba1f5342547095f611ae4ce4f7ded00b4223
Ben Alman <cowboy@rj3.net>
2012-01-09 14:49:49 -0500

    Removing init stuff from master. It'll be in a branch until I can get it figured out.

--- extras/init/jquery/test/<%PROJECTNAME%>.js
+++
@@ -1,9 +0,0 @@
-/*global $:true */
-/*global QUnit:true, module:true, test:true, asyncTest:true, expect:true,
-  ok:true, equal:true, notEqual:true, deepEqual:true, notDeepEqual:true,
-  strictEqual:true, notStrictEqual:true, raises:true, start:true, stop:true */
-
-test('test_name', 1, function() {
-  var fix = $('#qunit-fixture');
-  deepEqual(fix.awesome().get(), fix.get(), 'method should be chainable');
-});

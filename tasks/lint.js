ab9400f9ae8e4c51b61927dc32713026fd13df6b
Ben Alman <cowboy@rj3.net>
2012-02-01 11:32:55 -0500

    Tweaking lint output to highlight the offending character in red.

--- tasks/lint.js
+++ tasks/lint.js
@@ -38,6 +38,23 @@ task.registerBasicTask('lint', 'Validate files with JSHint.', function(data, nam
 // HELPERS
 // ============================================================================
 
+// No idea why JSHint treats tabs as options.indent # characters wide, but it
+// does. See issue: https://github.com/jshint/jshint/issues/430
+function getTabStr(options) {
+  // Do something that's going to error.
+  jshint('\tx', options || {});
+  // If an error occurred, figure out what character JSHint reported and
+  // subtract one.
+  var character = jshint.errors && jshint.errors[0] && jshint.errors[0].character - 1;
+  // If character is actually a number, use it. Otherwise use 1.
+  var tabsize = isNaN(character) ? 1 : character;
+  // If tabsize > 1, return something that should be safe to use as a
+  // placeholder. \uFFFF repeated 2+ times.
+  return tabsize > 1 && util.repeat(tabsize, '\uFFFF');
+}
+
+var tabregex = /\t/g;
+
 // Lint source code with JSHint.
 task.registerHelper('lint', function(src, options, globals, extraMsg) {
   // JSHint sometimes modifies objects you pass in, so clone them.
@@ -49,6 +66,9 @@ task.registerHelper('lint', function(src, options, globals, extraMsg) {
   }
   var msg = 'Linting' + (extraMsg ? ' ' + extraMsg : '') + '...';
   verbose.write(msg);
+  // Tab size as reported by JSHint.
+  var tabstr = getTabStr(options);
+  var placeholderregex = new RegExp(tabstr, 'g');
   // Lint.
   var result = jshint(src, options || {}, globals || {});
   // Attempt to work around JSHint erroneously reporting bugs.
@@ -73,13 +93,30 @@ task.registerHelper('lint', function(src, options, globals, extraMsg) {
       // Sometimes there's no error object.
       if (!e) { return; }
       var pos;
-      if (e.evidence) {
+      var evidence = e.evidence;
+      var character = e.character;
+      if (evidence) {
         // Manually increment errorcount since we're not using log.error().
         fail.errorcount++;
         // Descriptive code error.
-        pos = '['.red + ('L' + e.line).yellow + ':'.red + ('C' + e.character).yellow + ']'.red;
+        pos = '['.red + ('L' + e.line).yellow + ':'.red + ('C' + character).yellow + ']'.red;
         log.writeln(pos + ' ' + e.reason.yellow);
-        log.writeln(e.evidence.replace(/\t/g, '  ').inverse);
+        // If necessary, eplace each tab char with something that can be
+        // swapped out later.
+        if (tabstr) {
+          evidence = evidence.replace(tabregex, tabstr);
+        }
+        if (character > evidence.length) {
+          // End of line.
+          evidence = evidence + ' '.inverse.red;
+        } else {
+          // Middle of line.
+          evidence = evidence.slice(0, character - 1) + evidence[character - 1].inverse.red +
+            evidence.slice(character);
+        }
+        // Replace tab placeholder (or tabs) but with a 2-space soft tab.
+        evidence = evidence.replace(tabstr ? placeholderregex : tabregex, '  ');
+        log.writeln(evidence);
       } else {
         // Generic "Whoops, too many errors" error.
         log.error(e.reason);

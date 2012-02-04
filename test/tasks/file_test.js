bf556c15afdbafe68e9265b6a57f421ce5bfb04e
Mickael Daniel <daniel.mickael@gmail.com>
2012-02-04 08:36:12 +0100

    Adding tests for file read / write / copy

---
+++ test/tasks/file_test.js
@@ -0,0 +1,54 @@
+
+var fs = require('fs');
+
+exports['file'] = {
+  'read': function(test) {
+    test.expect(3);
+    test.equal(file.read('test/fixtures/a.js'), fs.readFileSync('test/fixtures/a.js', 'utf8'));
+    test.strictEqual(file.read('test/fixtures/a.js'), fs.readFileSync('test/fixtures/a.js', 'utf8'));
+    test.strictEqual(file.read('test/fixtures/octocat.png'), fs.readFileSync('test/fixtures/octocat.png', 'utf8'));
+    test.done();
+  },
+  'write': function(test) {
+    test.expect(4);
+    var content = 'var a = "foobar";';
+    file.write('test/fixtures/test_write.js', content);
+    test.strictEqual(fs.readFileSync('test/fixtures/test_write.js', 'utf8'), content);
+    test.strictEqual(file.read('test/fixtures/test_write.js'), content);
+
+    var octocat = fs.readFileSync('test/fixtures/octocat.png');
+    file.write('test/fixtures/test_write.png', octocat);
+    test.strictEqual(fs.readFileSync('test/fixtures/test_write.png', 'utf8'), fs.readFileSync('test/fixtures/octocat.png', 'utf8'));
+    test.ok(compare('test/fixtures/test_write.png', 'test/fixtures/octocat.png'), 'should both raw buffers match');
+
+    ['test/fixtures/test_write.js', 'test/fixtures/test_write.png'].forEach(fs.unlinkSync);
+    test.done();
+  },
+
+  'copy': function(test) {
+    test.expect(3);
+    file.copy('test/fixtures/a.js', 'test/fixtures/test_copy.js');
+    test.strictEqual(fs.readFileSync('test/fixtures/test_copy.js', 'utf8'), fs.readFileSync('test/fixtures/a.js', 'utf8'));
+
+    file.copy('test/fixtures/octocat.png', 'test/fixtures/test_copy.png');
+    test.strictEqual(fs.readFileSync('test/fixtures/test_copy.png', 'utf8'), fs.readFileSync('test/fixtures/octocat.png', 'utf8'));
+    test.ok(compare('test/fixtures/test_copy.png', 'test/fixtures/octocat.png'), 'should both raw buffers match');
+
+    ['test/fixtures/test_copy.js', 'test/fixtures/test_copy.png'].forEach(fs.unlinkSync);
+    test.done();
+  }
+};
+
+
+// test helper
+
+// compare - to effectively compare Buffers, we would need something like
+// bnoordhuis/buffertools, but I'd rather not add a new dependency for the sake
+// of testing.
+//
+// So we're relying on comparisons between the `hex` of buffers to do that,
+// seems to be reliant enough to cover our test needs with file copy.
+function compare(actual, expected, encoding) {
+  encoding = encoding || 'hex';
+  return fs.readFileSync(actual, encoding) === fs.readFileSync(expected, encoding);
+}

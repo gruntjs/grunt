38dda3d083886938683117b1fa079c7219b8c424
Ben Alman <cowboy@rj3.net>
2012-03-24 22:14:28 -0400

    Adding log.errorlns and log.oklns as a nice way to format multiple error / ok lines with auto-wrapping at 80 columns.

--- tasks/qunit.js
+++ tasks/qunit.js
@@ -248,14 +248,14 @@ module.exports = function(grunt) {
       verbose.or.writeln();
       log.write('Running PhantomJS...').error();
       if (code === 127) {
-        log.error(log.wraptext(77,
+        log.errorlns(
           'In order for this task to work properly, PhantomJS must be ' +
           'installed and in the system PATH (if you can run "phantomjs" at' +
           ' the command line, this task should work). Unfortunately, ' +
           'PhantomJS cannot be installed automatically via npm or grunt. ' +
           'See the grunt FAQ for PhantomJS installation instructions: ' +
           'https://github.com/cowboy/grunt/blob/master/docs/faq.md'
-        ));
+        );
         grunt.warn('PhantomJS not found.', options.code);
       } else {
         result.split('\n').forEach(log.error, log);

a6689314d3c3ef328526ac3f71b9ab7f2c3916bf
Ben Alman <cowboy@rj3.net>
2012-04-13 20:37:40 -0400

    Updating Underscore.js to 1.3.3. Closes #113.
    Note that any existing grunt templates containing \n or \r will be broken per documentcloud/underscore#553. If this error is encountered, a helpful error message will displayed for the user, who will hopefully not hate me.

--- tasks/init/commonjs/root/grunt.js
+++ tasks/init/commonjs/root/grunt.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     meta: {
       banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },
--- tasks/init/jquery/root/grunt.js
+++ tasks/init/jquery/root/grunt.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
     meta: {
       banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
     },

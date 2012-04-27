fd00ab734dac51f7a716c78c6a7052f38a3d736a
Ben Alman <cowboy@rj3.net>
2012-04-27 10:57:35 -0400

    Making the "jquery" init template plugin more awesome.

--- tasks/init/jquery/root/test/name_test.js
+++ tasks/init/jquery/root/test/name_test.js
@@ -23,33 +23,40 @@
   */
 
   module('jQuery#awesome', {
+    // This will run before each test in this module.
     setup: function() {
       this.elems = $('#qunit-fixture').children();
     }
   });
 
-  test('is chainable', 1, function() {
+  test('is chainable', function() {
+    expect(1);
     // Not a bad test to run on collection methods.
-    strictEqual(this.elems.awesome(), this.elems, 'should be chaninable');
+    strictEqual(this.elems.awesome(), this.elems, 'should be chainable');
   });
 
-  test('is awesome', 1, function() {
-    strictEqual(this.elems.awesome().text(), 'awesomeawesomeawesome', 'should be thoroughly awesome');
+  test('is awesome', function() {
+    expect(1);
+    strictEqual(this.elems.awesome().text(), 'awesome0awesome1awesome2', 'should be awesome');
   });
 
   module('jQuery.awesome');
 
-  test('is awesome', 1, function() {
-    strictEqual($.awesome(), 'awesome', 'should be thoroughly awesome');
+  test('is awesome', function() {
+    expect(2);
+    strictEqual($.awesome(), 'awesome.', 'should be awesome');
+    strictEqual($.awesome({punctuation: '!'}), 'awesome!', 'should be thoroughly awesome');
   });
 
   module(':awesome selector', {
+    // This will run before each test in this module.
     setup: function() {
       this.elems = $('#qunit-fixture').children();
     }
   });
 
-  test('is awesome', 1, function() {
+  test('is awesome', function() {
+    expect(1);
     // Use deepEqual & .get() when comparing jQuery objects.
     deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
   });

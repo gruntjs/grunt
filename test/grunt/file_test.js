4bf1fd7de396c70b6ebbcf8abefde2f624f33e1e
Dan Heberden <danheberden@gmail.com>
2012-05-25 13:49:15 -0700

    Add negation of wildcards test

--- test/grunt/file_test.js
+++ test/grunt/file_test.js
@@ -163,10 +163,11 @@ exports['file.expand*'] = {
     test.done();
   },
   'exclude': function(test) {
-    test.expect(3);
+    test.expect(4);
     test.deepEqual(grunt.file.expand(['js/bar.js','!js/bar.js']), [], 'negation should cancel match');
     test.deepEqual(grunt.file.expand(['**/*.js', '!js/foo.js']), ['js/bar.js'], 'should omit single file from matched set');
-    test.deepEqual(grunt.file.expand(['**/*.js', '**/*.css', '!js/bar.js', '!css/baz.css']), ['js/foo.js','css/qux.css'], 'multiple negations should be removed from list');
+    test.deepEqual(grunt.file.expand(['**/*.js', '**/*.css', '!js/bar.js', '!css/baz.css']), ['js/foo.js','css/qux.css'], 'multiple negations should be removed from the set');
+    test.deepEqual(grunt.file.expand(['**/*.js', '**/*.css', '!**/*.css']), ['js/bar.js', 'js/foo.js'], 'negated wildcards should be removed from the matched set');
     test.done()
   }
 };

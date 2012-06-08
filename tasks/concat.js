19a7f280b126f5e1915d82b3c803421bf3e695cf
Ben Alman <cowboy@rj3.net>
2012-06-08 12:14:34 -0400

    Update concat task to use the new task/target options property.

--- tasks/concat.js
+++ tasks/concat.js
@@ -16,9 +16,13 @@ module.exports = function(grunt) {
   // ==========================================================================
 
   grunt.registerMultiTask('concat', 'Concatenate files.', function() {
+    // Get any task- or target-specific options.
+    var options = this.options();
+    // The source files to be concatenated.
     var files = grunt.file.expandFiles(this.file.src);
     // Concat specified files.
-    var src = grunt.helper('concat', files, {separator: this.data.separator});
+    var src = grunt.helper('concat', files, {separator: options.separator});
+    // Write the destination file.
     grunt.file.write(this.file.dest, src);
 
     // Fail task if errors were logged.
@@ -34,10 +38,15 @@ module.exports = function(grunt) {
 
   // Concat source files and/or directives.
   grunt.registerHelper('concat', function(files, options) {
+    // Passed options override default options.
     options = grunt.util._.defaults(options || {}, {
       separator: grunt.util.linefeed
     });
+    // If files were specified, iterate over each file, joining them on the
+    // specified separator (or the default).
     return files ? files.map(function(filepath) {
+      // If the filepath is a directive, return its expanded value, otherwise
+      // read the filepath's contents and return that.
       return grunt.task.directive(filepath, grunt.file.read);
     }).join(grunt.util.normalizelf(options.separator)) : '';
   });

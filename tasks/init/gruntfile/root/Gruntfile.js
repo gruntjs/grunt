6d198f9c914299a55e841e5868be67d9dad3a04f
Ben Alman <cowboy@rj3.net>
2012-06-16 17:01:08 -0500

    Updating init templates to use new banner property instead of a directive in the src file list.

--- tasks/init/gruntfile/root/Gruntfile.js
+++ tasks/init/gruntfile/root/Gruntfile.js
@@ -4,21 +4,19 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({{% if (min_concat) { if (package_json) { %}
     pkg: '<json:package.json>',
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',{% } else { %}
     meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n'
-    },{% } else { %}
-    meta: {
-      version: '0.1.0',
-      banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '* http://PROJECT_WEBSITE/\n' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
-        'YOUR_NAME; Licensed MIT */\n'
-    },{% } } %}
+      version: '0.1.0'
+    },
+    banner: '/*! PROJECT_NAME - v<%= meta.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '* http://PROJECT_WEBSITE/\n' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+      'YOUR_NAME; Licensed MIT */\n',{% } } %}
     lint: {
       files: ['Gruntfile.js', '{%= lib_dir %}/**/*.js', '{%= test_dir %}/**/*.js']
     },{% if (dom) { %}
@@ -30,13 +28,13 @@ module.exports = function(grunt) {
     },{% } %}{% if (min_concat) { %}
     concat: {
       dist: {
-        src: ['<config_process:meta.banner>', '<file_strip_banner:{%= lib_dir %}/{%= file_name %}.js>'],
+        src: ['<file_strip_banner:{%= lib_dir %}/{%= file_name %}.js>'],
         dest: 'dist/{%= file_name %}.js'
       }
     },
     min: {
       dist: {
-        src: ['<config_process:meta.banner>', '<config:concat.dist.dest>'],
+        src: ['<config:concat.dist.dest>'],
         dest: 'dist/{%= file_name %}.min.js'
       }
     },{% } %}

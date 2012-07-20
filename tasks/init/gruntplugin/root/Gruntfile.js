4fb43cad69fd59b58274a6cc2e8dee02f7bd1f9a
Kyle Robinson Young <kyle@dontkry.com>
2012-07-20 11:54:07 -0400

    Use new multi task "options" object in init template-generated gruntfiles. Closes gh-290.

--- tasks/init/gruntplugin/root/Gruntfile.js
+++ tasks/init/gruntplugin/root/Gruntfile.js
@@ -8,28 +8,28 @@ module.exports = function(grunt) {
       files: ['test/**/*.js']
     },
     lint: {
-      files: ['Gruntfile.js', 'tasks/**/*.js', 'test/**/*.js']
+      files: ['Gruntfile.js', 'tasks/**/*.js', 'test/**/*.js'],
+      options: {
+        options: {
+          curly: true,
+          eqeqeq: true,
+          immed: true,
+          latedef: true,
+          newcap: true,
+          noarg: true,
+          sub: true,
+          undef: true,
+          boss: true,
+          eqnull: true,
+          node: true,
+          es5: true
+        },
+        globals: {}
+      }
     },
     watch: {
       files: '<config:lint.files>',
       tasks: 'default'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true,
-        es5: true
-      },
-      globals: {}
     }
   });
 

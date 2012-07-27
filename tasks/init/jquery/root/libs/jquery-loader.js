c4009aed26b2b24aaa2ef723952ad65228675882
Ben Alman <cowboy@rj3.net>
2012-07-27 14:50:35 -0400

    Minor jquery init template tweaks.

--- tasks/init/jquery/root/libs/jquery-loader.js
+++ tasks/init/jquery/root/libs/jquery-loader.js
@@ -1,13 +1,11 @@
 (function() {
+  // Default to the local version.
+  var path = '../libs/jquery/jquery.js';
   // Get any jquery=___ param from the query string.
   var jqversion = location.search.match(/[?&]jquery=(.*?)(?=&|$)/);
-  var path;
+  // If a version was specified, use that version from code.jquery.com.
   if (jqversion) {
-    // A version was specified, load that version from code.jquery.com.
     path = 'http://code.jquery.com/jquery-' + jqversion[1] + '.js';
-  } else {
-    // No version was specified, load the local version.
-    path = '../libs/jquery/jquery.js';
   }
   // This is the only time I'll ever use document.write, I promise!
   document.write('<script src="' + path + '"></script>');

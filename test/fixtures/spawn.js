7f3c26b06c8039d1cad3a3de9bde9aec3637e768
Ben Alman <cowboy@rj3.net>
2012-08-22 13:20:58 -0400

    Attempt to work around Node.js exiting before stdout/stderr are flushed in Windows.

--- test/fixtures/spawn.js
+++ test/fixtures/spawn.js
@@ -4,4 +4,6 @@ var code = Number(process.argv[2]);
 process.stdout.write('stdout\n');
 process.stderr.write('stderr\n');
 
-process.exit(code);
+// Use instead of process.exit to ensure stdout/stderr are flushed
+// before exiting in Windows (Tested in Node.js v0.8.7)
+require('../../lib/util/exit').exit(code);

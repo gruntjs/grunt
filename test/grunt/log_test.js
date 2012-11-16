108257ce85ad5362c2f7e71f2bf70d8edc177690
Ben Alman <cowboy@rj3.net>
2012-11-16 17:05:59 -0500

    Remove unnecessary test init code.

--- test/grunt/log_test.js
+++ test/grunt/log_test.js
@@ -2,13 +2,6 @@
 
 var grunt = require('../../lib/grunt');
 
-// In case the grunt being used to test is different than the grunt being
-// tested, initialize the task and config subsystems.
-if (grunt.task.searchDirs.length === 0) {
-  grunt.task.init([]);
-  grunt.config.init({});
-}
-
 // Helper for testing stdout
 var hooker = grunt.util.hooker;
 var stdoutEqual = function(test, callback, expected) {
--- test/grunt/task_test.js
+++ test/grunt/task_test.js
@@ -2,13 +2,6 @@
 
 var grunt = require('../../lib/grunt');
 
-// In case the grunt being used to test is different than the grunt being
-// tested, initialize the task and config subsystems.
-if (grunt.task.searchDirs.length === 0) {
-  grunt.task.init([]);
-  grunt.config.init({});
-}
-
 exports['task.normalizeMultiTaskFiles'] = {
   setUp: function(done) {
     this.cwd = process.cwd();

b0bd0751ceb1a8a3237c12ba16d985c79c1aeeca
Ben Alman <cowboy@rj3.net>
2012-12-11 16:28:18 -0500

    Removing init/user {% %} [% %] templates by default (they can still be added in manually if needed).

--- test/grunt/template_test.js
+++ test/grunt/template_test.js
@@ -20,42 +20,28 @@ exports['template'] = {
     test.done();
   },
 
-  'init mode': function(test) {
-    test.expect(5);
+  'custom delimiters': function(test) {
+    test.expect(6);
     var obj = {
       foo: 'c',
       bar: 'b{%= foo %}d',
       baz: 'a{%= bar %}e'
     };
 
-    test.equal(grunt.template.process('{%= foo %}', {data: obj, delimiters: 'init'}), 'c', 'should retrieve value.');
-    test.equal(grunt.template.process('{%= bar %}', {data: obj, delimiters: 'init'}), 'bcd', 'should recurse.');
-    test.equal(grunt.template.process('{%= baz %}', {data: obj, delimiters: 'init'}), 'abcde', 'should recurse.');
+    test.equal(grunt.template.process('{%= foo %}', {data: obj, delimiters: 'custom'}), '{%= foo %}', 'custom delimiters have yet to be defined.');
 
-    test.equal(grunt.template.process('{%= foo %}<%= foo %>', {data: obj, delimiters: 'init'}), 'c<%= foo %>', 'should ignore default delimiters');
+    // Define custom delimiters.
+    grunt.template.addDelimiters('custom', '{%', '%}');
+
+    test.equal(grunt.template.process('{%= foo %}', {data: obj, delimiters: 'custom'}), 'c', 'should retrieve value.');
+    test.equal(grunt.template.process('{%= bar %}', {data: obj, delimiters: 'custom'}), 'bcd', 'should recurse.');
+    test.equal(grunt.template.process('{%= baz %}', {data: obj, delimiters: 'custom'}), 'abcde', 'should recurse.');
+
+    test.equal(grunt.template.process('{%= foo %}<%= foo %>', {data: obj, delimiters: 'custom'}), 'c<%= foo %>', 'should ignore default delimiters');
 
     obj.foo = '{% oops %';
-    test.equal(grunt.template.process('{%= baz %}', {data: obj, delimiters: 'init'}), 'ab{% oops %de', 'should not explode.');
+    test.equal(grunt.template.process('{%= baz %}', {data: obj, delimiters: 'custom'}), 'ab{% oops %de', 'should not explode.');
 
     test.done();
   },
-
-  'user mode': function(test) {
-    test.expect(5);
-    var obj = {
-      foo: 'c',
-      bar: 'b[%= foo %]d',
-      baz: 'a[%= bar %]e'
-    };
-
-    test.equal(grunt.template.process('[%= foo %]', {data: obj, delimiters: 'user'}), 'c', 'should retrieve value.');
-    test.equal(grunt.template.process('[%= bar %]', {data: obj, delimiters: 'user'}), 'bcd', 'should recurse.');
-    test.equal(grunt.template.process('[%= baz %]', {data: obj, delimiters: 'user'}), 'abcde', 'should recurse.');
-
-    test.equal(grunt.template.process('[%= foo %]<%= foo %>', {data: obj, delimiters: 'user'}), 'c<%= foo %>', 'should ignore default delimiters');
-
-    obj.foo = '[% oops %';
-    test.equal(grunt.template.process('[%= baz %]', {data: obj, delimiters: 'user'}), 'ab[% oops %de', 'should not explode.');
-    test.done();
-  }
 };

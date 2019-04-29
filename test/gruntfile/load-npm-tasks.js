66fc8fa4409a1c46f3ff8ff60782eaa827c94328
Vadim Tomnikov <vadim.tomnikov@sap.com>
2019-04-29 23:57:20 +0300

    support monorepo (test case)
    add integration test for monorepo case, where grunt plugin hoisted to
    the `node_modules` above package having dev dependency to it

--- test/gruntfile/load-npm-tasks.js
+++ test/gruntfile/load-npm-tasks.js
@@ -4,8 +4,8 @@ var Log = require('grunt-legacy-log').Log;
 var assert = require('assert');
 var through = require('through2');
 
-module.exports = function(grunt) {
-  grunt.file.setBase('../fixtures/load-npm-tasks');
+function test(grunt, fixture) {
+  grunt.file.setBase('../fixtures/' + fixture);
 
   // Create a custom log to assert output
   var stdout = [];
@@ -39,4 +39,11 @@ module.exports = function(grunt) {
       throw err;
     }
   });
+}
+
+module.exports = function(grunt) {
+  // NPM task package is inside $CWD/node_modules
+  test(grunt, 'load-npm-tasks');
+  // NPM task package hoisted to $CWD/../node_modules
+  test(grunt, 'load-npm-tasks/test-package');
 };
